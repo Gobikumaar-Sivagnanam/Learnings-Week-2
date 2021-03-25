5db0d7166e4990ae66ef166e4d65242dec7c3b04
Gobikumaar-Sivagnanam <gobikumaar16@gmail.com>
2021-03-25 21:02:22 +0530

    Added Documentation

--- test.c
+++ test.c
@@ -12,22 +12,6 @@
 #include "funp.h"
 #include "unity_internals.h"
 #include "unity.h"
-/* Sample program to test functions
-int main(){
-    complex_t num1 = {2.00,3.00}, num2 = {1.00,2.00}, a, s, m, d;
-    a = addition(num1, num2);
-    s = difference(num1, num2);
-    m = product(num1, num2);
-    d = division(num1, num2);
-    printf("SUM IS : %f + %f \n",a.real,a.imaginary);
-    printf("DIFFERENCE IS : %f + %f \n",s.real,s.imaginary);
-    printf("PRODUCT IS : %f + %f \n",m.real,m.imaginary);
-    printf("QUOTIENT IS : %f + %f \n",d.real,d.imaginary);
-    return 0;
-}
-*/
-
-// Unity Test Program
 
 // Setup Functions for Unity
 void setUp(){
@@ -127,9 +111,9 @@ void division_test(void){
 
 }
 /**
- * @brief Main Driver 
- * 
- * @return int 
+ * @brief Main Driver for Unity Testing 
+ * Testing the different fucntions.
+ * @return UNITY END
  */
 int main(void){
 	UNITY_BEGIN();
